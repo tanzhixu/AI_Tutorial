3b7e11a99f6aa54ea42b9524d24944c406e20eda
tanzhixu <tanzhixu@aliyun.com>
2024-05-06 21:38:25 +0800

    'UPDATE'

---
+++ 06_RAG/Rag_For_Docs/compare_embeddings.py
@@ -0,0 +1,21 @@
+from langchain_community.embeddings import OllamaEmbeddings
+from langchain_community.embeddings import HuggingFaceEmbeddings
+from langchain.evaluation import load_evaluator
+
+
+def main():
+    # Get embedding for a word.
+    embedding_function = HuggingFaceEmbeddings()
+    vector = embedding_function.embed_query("apple")
+    print(f"Vector for 'apple': {vector}")
+    print(f"Vector length: {len(vector)}")
+
+    # Compare vector of two words
+    evaluator = load_evaluator("pairwise_embedding_distance")
+    words = ("apple", "iphone")
+    x = evaluator.evaluate_string_pairs(prediction=words[0], prediction_b=words[1])
+    print(f"Comparing ({words[0]}, {words[1]}): {x}")
+
+
+if __name__ == "__main__":
+    main()
